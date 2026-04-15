1272c4111bdfd30a214b374fb2fb775c9670b016
Михаил Иващенко <spac1xx.32@gmail.com>
2026-04-15 19:28:47 +0300

    def

--- game.py
+++ game.py
@@ -16,13 +16,15 @@ class Board:
             print('-' * 5)
 
 
+
 from gameparts import Board
 
-# Всё, что ниже этой инструкции, не будет импортироваться,
-# но будет выполняться при запуске файла game.py.
-if __name__ == '__main__':
+def main():
     game = Board()
     game.display()
     game.make_move(1, 1, 'X')
     print('Ход сделан!')
     game.display()
+
+if __name__ == '__main__':
+    main()
