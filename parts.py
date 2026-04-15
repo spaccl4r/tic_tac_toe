3e5e7a47675c4dd160989085af344bf8d75f8f76
Михаил Иващенко <spac1xx.32@gmail.com>
2026-04-15 20:21:16 +0300

    описание класса

--- parts.py
+++ parts.py
@@ -1,8 +1,14 @@
-# parts.py
+# gameparts/parts.py
 
 class Board:
+    """Класс, который описывает игровое поле."""
+
+    field_size = 3
+
     def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
 
     def make_move(self, row, col, player):
         self.board[row][col] = player
@@ -11,3 +17,11 @@ class Board:
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
+
+print(Board.__doc__)
